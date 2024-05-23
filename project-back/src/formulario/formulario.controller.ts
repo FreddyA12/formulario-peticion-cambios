dbad1f629bda3910fd7314f651c90db5cd08ffa5
nanobotrs <raansopro324@gmail.com>
2024-05-23 01:23:34 -0500

    Creacion de entidad, controller, service, module de formularios.
    Co-authored-by: Freddy Alvarez <FreddyA12@users.noreply.github.com>

---
+++ project-back/src/formulario/formulario.controller.ts
@@ -0,0 +1,58 @@
+import { Body, Controller, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { FormularioService } from './formulario.service';
+import { Formulario } from './formulario.entity';
+
+@Controller('formulario')
+export class FormularioController {
+    constructor(private formularioService: FormularioService) { }
+
+    @Get()
+    async getAll(): Promise<Formulario[]> {
+        return await this.formularioService.getAll();
+    }
+
+    @Get()
+    async getById(@Query('id') id: number) {
+        return await this.formularioService.getById(id);
+    }
+
+    @Post()
+    async save(@Body() formulario: Formulario) {
+        return await this.formularioService.save(formulario);
+    }
+
+    @Patch('/update')
+    async update(@Query('id') id: number, @Body() formulario: any) {
+        return await this.formularioService.update(id, formulario);
+    }
+
+    // Métodos para obtener formularios ordenados
+    @Get("/ordenados")
+    async getOrdered(@Query('por') por: string): Promise<Formulario[]> {
+        switch (por) {
+            case 'prioridad':
+                return this.formularioService.getAllOrdered('prioridad');
+            case 'fecha':
+                return this.formularioService.getAllOrdered('fecha_solicitud');
+            case 'estado':
+                return this.formularioService.getAllOrdered('estado');
+            default:
+                return this.formularioService.getAll();
+        }
+    }
+    // TODO:
+    // traer ordenado por:
+    // por prioridad
+    // http://localhost:3000/formulario/ordenados?por=prioridad
+
+    // por fecha
+    // por estado
+
+    // Get va con PathVariable
+    // Put o Patch va con PathVariable y request body
+    // Delete va con PathVariable
+
+}
+
+
+
---
+++ project-back/src/formulario/formulario.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Formulario } from './formulario.entity';
+
+@Injectable()
+export class FormularioService {
+
+
+  constructor(
+    @InjectRepository(Formulario)
+    private formularioRepository: Repository<Formulario>,
+  ) { }
+
+  async getById(id: number) {
+    return await this.formularioRepository.findOne({ where: { numero_de_solicitud: id } })
+  }
+
+  async save(formularioData: any) {
+    return await this.formularioRepository.save(formularioData)
+  }
+
+  async getAll(): Promise<Formulario[]> {
+    return await this.formularioRepository.find();
+  }
+
+  async update(id: number, formularioData: any): Promise<Formulario> {
+    console.log('1');
+    const formulario = await this.getById(id);
+    if (!formulario) {
+      throw new Error('Formulario not found');
+    }
+    Object.assign(formulario, formularioData);
+    return this.formularioRepository.save(formulario);
+  }
+
+  async getAllOrdered(orderBy: string): Promise<Formulario[]> {
+    if (orderBy === 'prioridad') {
+      return this.formularioRepository
+        .createQueryBuilder('formulario')
+        .orderBy('CASE ' +
+          'WHEN formulario.prioridad = \'alta\' THEN 1 ' +
+          'WHEN formulario.prioridad = \'media\' THEN 2 ' +
+          'WHEN formulario.prioridad = \'baja\' THEN 3 ' +
+          'ELSE 4 END', 'ASC')
+        .getMany();
+    } else if (orderBy === 'fecha') {
+      return this.formularioRepository
+        .createQueryBuilder('formulario')
+        .orderBy('formulario.fecha_solicitud', 'DESC') // Orden descendente para las fechas más recientes
+        .getMany();
+    } else {  // Manejo de otros tipos de ordenamiento, si es necesario
+      const orderCriteria = { [orderBy]: 'ASC' }; // Ajusta según necesidad
+      return await this.formularioRepository.find({ order: orderCriteria });
+    }
+  }
+
+}
